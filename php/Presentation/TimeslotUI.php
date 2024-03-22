238a65f2f1e21667d91dee0f4bedcbd88c1fe746
Childish Wu <david.a.malcolms@gmail.com>
2024-03-22 07:48:51 -0500

    refactored changes

---
+++ php/Presentation/TimeslotUI.php
@@ -0,0 +1,48 @@
+<?php
+require '../Business Logic/TimeslotController.php';
+Class TimeslotUI{
+    private $machineNum;
+    function __construct() {
+        if (isset($_POST['machineNum'])){
+            $machineNum = $_POST['machineNum'];
+            $this->machineNum = $machineNum;
+        }
+        
+    }
+
+    public function showMachineStatus($machineKey){
+        $controller = new TimeslotController();
+        return $controller->fetchStatus($machineKey);
+    }
+    public function removeTimePassed($machineKey,$timeslot,$selectedDay){
+        $controller = new TimeslotController();
+        return $controller->timeslotRemover($machineKey,$timeslot,$selectedDay);
+    }
+    public function assignTime($machineKey,$timeslot,$selectedDay){
+        $controller = new TimeslotController();
+        return $controller->assignTimeslot($machineKey,$timeslot,$selectedDay);
+    }
+
+    public function reservationsForDay($selectedDay){
+        $controller = new TimeslotController();
+        return $controller->dailyReservations($selectedDay);
+    }
+
+    public function reservationsForcheck($selectedDay){
+        $controller = new TimeslotController();
+        return $controller->checkingReservations($selectedDay);
+    }
+
+} 
+
+$handle =  new TimeslotUI();
+
+if (isset($_POST['timeslot'],$_POST['machine'],$_POST['selectedDay'])){
+    echo $handle->assignTime($_POST['machine'],$_POST['timeslot'],$_POST['selectedDay']);
+}
+
+elseif (isset($_POST['selectedDay'])){
+    echo $handle->reservationsForDay($_POST['selectedDay']);
+}
+
+
