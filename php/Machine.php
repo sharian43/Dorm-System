00829ae70abf345d6cc6a0a28aeb42e6fe05e93b
Kerice Forde <145314259+kericeforde@users.noreply.github.com>
2024-03-21 21:06:36 -0500

    Create Machine.php

---
+++ php/Machine.php
@@ -0,0 +1,28 @@
+<?php
+class Machine{
+  private $machine;
+  private $status;
+
+   public function __construct($machine,$status){
+      $this->$machine=$machine;
+      $this->$status=$status;
+   }
+  public function getMachine(){
+      return $this->$machine; }
+   public function getStatus(){
+      return $this->$status;}
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
